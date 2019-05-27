6404bf1f14df47a7c7cad8c71cca00c232fcc1b5
geekape <861013016@qq.com>
2019-05-27 08:47:04 +0800

    update: vue双向滚动绑定

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,63 +1,70 @@
 
 <template>
-	<div class="index container">
-    <el-row　class="left-bar" :style="{left: isLeftbar ? 0 : '-249px'}">
+  <section class="index container">
+    <div class="left-bar" :style="{left: isLeftbar ? 0 : '-249px'}">
       <div class="title">
-				<img class="logo" src="/favicon.ico" />
+        <img class="logo" src="/favicon.ico">
         <span>猿梦极客导航</span>
-			</div>
-      <el-col :span="24">
-        <el-menu
-        default-active="0"
-          class="el-menu-vertical-demo"
-          background-color="#30333c"
-          text-color="#6b7386"
-          active-text-color="#fff"
+      </div>
+      <el-row>
+        <el-col :span="24">
+          <el-menu
+            :default-active="active"
+            class="el-menu-vertical-demo"
+            background-color="#30333c"
+            text-color="#6b7386"
+            active-text-color="#fff"
           >
-          <el-menu-item :index="item._id"　v-for="(item,index) in data" :key="index" @click="jump(index)">
-            <i :class="item.icon"></i>
-            <span slot="title">{{item.classify}}</span>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
-  </el-row>
-	
-		<section class="main">
-			<div id="mainContent">
-				<!-- 手机端菜单 -->
-				<div id="menu-box">
-					<div id="menu">
-						<input type="checkbox" id="menu-form">
-						<label for="menu-form" class="menu-spin" @click="isLeftbar=!isLeftbar">
-							<div class="line diagonal line-1"></div>
-							<div class="line horizontal"></div>
-							<div class="line diagonal line-2"></div>
-						</label>
-					</div>
-				</div>
-				<!-- 开发社区 -->
-				<div class="box" v-for="(item,index) in data" :key="index">
-					<a href="#" :name="item.classify"></a>
-					<div class="sub-category">
-						<div>
-							<i :class="item.icon" class="icon"></i>
-							{{item.classify}}
-						</div>
-					</div>
-					<NavItem :data="sub"  v-for="(sub,idx) in item.sites" :key="'sub-'+idx" />
-				</div>
-			</div>
-			<footer class="footer">
-				<div class="copyright">
-					<div>
-						Copyright © 2018- 2050
-						<a href="http://geekape.github.io">极客猿梦导航 钟储兵博客</a>
-					</div>
-				</div>
-			</footer>
-			<back-top/>
-		</section><add-nav-btn :data="data" />
-	</div>
+            <el-menu-item
+              :index="index"
+              v-for="(item,index) in data"
+              :key="item._id"
+              @click="jump(index)"
+            >
+              <i :class="item.icon"></i>
+              <span slot="title">{{item.classify}}</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </div>
+    <section class="main">
+      <div id="mainContent">
+        <!-- 手机端菜单 -->
+        <div id="menu-box">
+          <div id="menu">
+            <input type="checkbox" id="menu-form">
+            <label for="menu-form" class="menu-spin" @click="isLeftbar=!isLeftbar">
+              <div class="line diagonal line-1"></div>
+              <div class="line horizontal"></div>
+              <div class="line diagonal line-2"></div>
+            </label>
+          </div>
+        </div>
+        <!-- 开发社区 -->
+        <div class="box" v-for="(item,index) in data" :key="index" :ref="`box-${index}`">
+          <a href="#" :name="item.classify"></a>
+          <div class="sub-category">
+            <div>
+              <i :class="item.icon" class="icon"></i>
+              {{item.classify}}
+            </div>
+          </div>
+          <NavItem :data="sub" v-for="(sub,idx) in item.sites" :key="'sub-'+idx"/>
+        </div>
+      </div>
+      <footer class="footer">
+        <div class="copyright">
+          <div>
+            Copyright © 2018- 2050
+            <a href="http://geekape.github.io">极客猿梦导航 钟储兵博客</a>
+          </div>
+        </div>
+      </footer>
+      <back-top/>
+    </section>
+    <add-nav-btn :data="data"/>
+  </section>
 </template>
 
 <script>
@@ -67,6 +74,7 @@ import NavItem from "@/components/NavItem";
 export default {
   data() {
     return {
+      active: "0",
       data: [],
       scroll: 0,
       selfIndex: 0,
@@ -75,10 +83,22 @@ export default {
   },
   components: {
     BackTop,
-		AddNavBtn,
-		NavItem
+    AddNavBtn,
+    NavItem
   },
-  computed: {},
+  watch: {
+    active () {
+      const leftBarTop = document.querySelector('.left-bar').scrollTop
+      const num = this.active
+      const length = this.data.length
+      if (num >= 10 && num <= length) {
+        document.querySelector('.left-bar').scrollTop = leftBarTop + 60
+      } 
+      if (num < 10 && num >= 0) {
+        document.querySelector('.left-bar').scrollTop = leftBarTop - 60
+      }
+    }
+  },
   methods: {
     jump(idx) {
       this.selfIndex = idx;
@@ -113,9 +133,23 @@ export default {
           that.selfIndex = i;
         }
       }
+    },
+    handleScroll () {
+      const that = this
+      const length = that.data.length
+      const content = document.querySelectorAll('.box')
+      const top = document.body.scrollTop || document.documentElement.scrollTop
+      for (let i=length-1; i>=0; i--) {
+        if (top >= content[i].offsetTop - 20) {
+          that.active = i.toString()
+          break
+        }
+      }
     }
   },
-
+  mounted (){
+    window.addEventListener('scroll', this.handleScroll);
+  },
   created() {
     const that = this;
     this.getData();
