c1c2e1c49f73af7f98212fac4c2ec361a7041cc4
geekape <861013016@qq.com>
2021-08-15 21:00:15 +0800

    test: 前台页面缓存问题

---
+++ geekape-nav-main/layouts/second.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+p {
+  margin: 0;
+}
+
+a[title="站长统计"] {
+  display: none;
+}
+
+</style>
