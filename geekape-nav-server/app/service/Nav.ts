3b34d3d23a93b7a0bf17ba2760abc503ca88795b
zhongchubing <zhongchubing@runstech.com>
2021-08-16 16:25:29 +0800

    fix: 排行榜只显示审核通过导航

--- geekape-nav-server/app/service/Nav.ts
+++ geekape-nav-server/app/service/Nav.ts
@@ -1,8 +1,14 @@
 import { Service } from 'egg';
 
+export enum NAV_STATUS {
+    pass,
+    wait,
+    reject
+}
+
 
 export default class NavService extends Service {
     async findMaxValueList(value) {
-        return await this.ctx.model.Nav.find().sort({ [value]: -1 }).limit(10)
+        return await this.ctx.model.Nav.find({ status: NAV_STATUS.pass }).sort({ [value]: -1 }).limit(10)
     }
 }
