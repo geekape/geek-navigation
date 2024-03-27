32265ba9885bbf7a43cb14d1b33557561bbe0d02
geekape <861013016@qq.com>
2024-03-27 11:57:29 +0800

    分类管理

--- app/service/nav.js
+++ app/service/nav.js
@@ -14,10 +14,10 @@ class NavService extends Service {
         const { knex, jianghuKnex } = this.ctx.app
         const { categoryId ="ai" } = actionData
 
-        // 查分类数据，以parentId为categoryId
-        const categoryList = await jianghuKnex('category').where({ parentId: categoryId }).select('*')
+        // 查分类数据，以parentId为categoryId，按sort排序，按status过滤掉0的数据
+        const categoryList = await jianghuKnex('category').where({ parentId: categoryId }).orderBy('sort', 'desc').where('status', '!=', 0).select('*')
         // 查所有导航数据
-        const navList = await jianghuKnex('nav').select('*')
+        const navList = await jianghuKnex('nav').orderBy('sort', 'desc').where('status', '!=', 0).select('*')
 
         // 遍历分类，给children加上导航数据
         categoryList.forEach((category) => {
